ab1b2b49c449bad906d17383cfc6c828eb5aec8e
Maggie Jo Saylor <saylor.mj@gmail.com>
2019-07-26 21:17:07 -0500

    created characters.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Discover from "./pages/Game";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
