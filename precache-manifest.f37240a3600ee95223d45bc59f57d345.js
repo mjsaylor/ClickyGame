1f936fcae6963f8ef468e99aebbe7afaaf942e55
Maggie Jo Saylor <saylor.mj@gmail.com>
2019-07-27 19:09:47 -0500

    Updates

---
+++ precache-manifest.f37240a3600ee95223d45bc59f57d345.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5876de3786a77b7bf8b67163c4242421",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "01879081fe488ae7b278",
+    "url": "/ClickyGame/static/css/main.bf05311e.chunk.css"
+  },
+  {
+    "revision": "08ca966498d955f1c83b",
+    "url": "/ClickyGame/static/js/2.3d8dcad6.chunk.js"
+  },
+  {
+    "revision": "01879081fe488ae7b278",
+    "url": "/ClickyGame/static/js/main.0e1490e4.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
